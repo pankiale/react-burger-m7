ca3f371db8be52857360a606b6d2d162c3568869
pankiale <93738391+pankiale@users.noreply.github.com>
2022-05-24 14:35:40 +0200

    commit 1

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,5 +1,9 @@
 import styles from './app-header.module.css'
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
+import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { Typography } from '@ya.praktikum/react-developer-burger-ui-components'
 
 const AppHeader = ({title, children}) => {
     return (
@@ -7,13 +11,13 @@ const AppHeader = ({title, children}) => {
                 <nav>
                     <ul className={styles.header__navigation}>
                         <li class="header__button">
-                            <p class="header__link">
-                                <img class="header__icon" src="images/courses-icon-active.svg" alt="Курсы"/>Курсы
+                            <p className='text text_type_main-default'>
+                               <BurgerIcon type="primary" /> Конструктор
                             </p>
                         </li>
                         <li class="header__button">
-                            <a class="header__link header__link_active" href="profile.html">
-                                <img class="header__icon" src="images/profile-icon.svg" alt="Профиль"/>Профиль
+                            <a className='text text_type_main-default text_color_inactive' href="profile.html">
+                            <ListIcon type="secondary" /> Лента Заказов
                             </a>
                         </li>
                     </ul>
@@ -22,7 +26,7 @@ const AppHeader = ({title, children}) => {
                 <nav>
                     <ul className={styles.header__login}>
                             <a class="header__link header__link_active" href="profile.html">
-                                <img class="header__icon" src="images/profile-icon.svg" alt="Профиль"/>Профиль
+                            <ProfileIcon type="secondary" />Профиль
                             </a>
                     </ul>
                 </nav>
