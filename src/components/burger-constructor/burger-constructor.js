694e51755b19eef20ebec5aa94e7b475faf19895
pankiale <93738391+pankiale@users.noreply.github.com>
2022-05-27 17:53:24 +0200

    final2

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
 import { DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import Card from "../card/card";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./burger-constructor.module.css";
 
 function BurgerConstructor(props) {
   props.data[0];
 
-  const IngredientList = ({ title, ingredient, props }) => {
+  /*const TotalBill = ({filter, props }) => {
+    return (
+      <div className={`${styles.ingredients__item} pl-4`}>
+        <div className="pr-3">
+          <ConstructorElement
+            type="top"
+            text={`${props.data[0].name} (верх)`}
+            price={props.data[0].price}
+            thumbnail={props.data[0].image}
+            isLocked={true}
+          />
+        </div>
+        </div>
+  )};*/
+
+  const IngredientList = ({filter, props }) => {
     return (
       <div className={`${styles.ingredients__item} pl-4`}>
         <div className="pr-3">
@@ -22,7 +37,7 @@ function BurgerConstructor(props) {
 
         <ul className={`${styles.ingredients__list} mt-4 mb-4 `}>
           {props.data
-            .filter((item) => item.type === ingredient)
+            .filter((item) => item.type === filter)
             .map((item) => {
               return (
                 <li className={styles.ingredients__el} key={item._id}>
@@ -52,7 +67,10 @@ function BurgerConstructor(props) {
 
   return (
     <section className={`${styles.ingredients__section} pl-5 pr-4 pt-25`}>
-      <IngredientList title="Соусы" ingredient="main" props={props} />
+      <IngredientList filter="main" props={props} />
+      <Button type="primary" size="large">
+        Оформить заказ
+      </Button>
     </section>
   );
 }
