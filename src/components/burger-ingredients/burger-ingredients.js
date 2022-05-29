570c37b9ada66fe1bc384e3265c4993c60c32ef8
pankiale <93738391+pankiale@users.noreply.github.com>
2022-05-29 19:23:52 +0200

    scrollIntoView

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import { dataTypes } from "../../utils/const";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
@@ -12,25 +12,35 @@ BurgerIngredients.propTypes = {
 
 function BurgerIngredients(props) {
   const [current, setCurrent] = React.useState("bun");
+  const bun = useRef(null);
+  const sauce = useRef(null);
+  const main = useRef(null);
+
+function handleClick (value) {
+  setCurrent(value);
+  value === 'bun' ? bun.current.scrollIntoView() 
+  : value === 'sauce' ? sauce.current.scrollIntoView() 
+  : main.current.scrollIntoView(); 
+}
 
   return (
     <section className={`${styles.ingredients__section} pl-4 pr-5 pt-10 pb-8`}>
       <h1 className="text text_type_main-large mb-5"> Соберите бургер</h1>
       <div style={{ display: "flex" }}>
-        <Tab value="bun" active={current === "bun"} onClick={setCurrent}>
+        <Tab value="bun" active={current === "bun"} onClick={handleClick}>
           Булки
         </Tab>
-        <Tab value="sauce" active={current === "sauce"} onClick={setCurrent}>
+        <Tab value="sauce" active={current === "sauce"} onClick={handleClick}>
           Соусы
         </Tab>
-        <Tab value="main" active={current === "main"} onClick={setCurrent}>
+        <Tab value="main" active={current === "main"} onClick={handleClick}>
           Начинка
         </Tab>
       </div>
       <div className={styles.ingredients__menu}>
-        <IngredientList title="Булки" ingredient="bun" data={props} />
-        <IngredientList title="Соусы" ingredient="sauce" data={props} />
-        <IngredientList title="Начинки" ingredient="main" data={props} />
+        <IngredientList ref = {bun} title="Булки" ingredient="bun" data={props} />
+        <IngredientList ref = {sauce} title="Соусы" ingredient="sauce" data={props} />
+        <IngredientList ref = {main} title="Начинки" ingredient="main" data={props} />
       </div>
     </section>
   );
--- src/components/ingredient-list/ingredient-list.js
+++ src/components/ingredient-list/ingredient-list.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { forwardRef } from "react";
 import PropTypes from "prop-types";
 import { dataTypes } from "../../utils/const";
 import Card from "../card/card";
 import styles from "./ingredient-list.module.css";
 
-const IngredientList = ({ title, ingredient, data }) => {
+const IngredientList = React.forwardRef (({ title, ingredient, data }, ref) => {
 
   return (
     <div className={styles.ingredients__item}>
-      <p className="text text_type_main-medium">{title}</p>
+      <p ref = {ref} className="text text_type_main-medium">{title}</p>
       <ul className={`${styles.ingredients__list} pl-4 pr-4 pt-6`}>
         {data.data
           .filter((item) => item.type === ingredient)
@@ -22,7 +23,7 @@ const IngredientList = ({ title, ingredient, data }) => {
       </ul>
     </div>
   );
-};
+});
 
 IngredientList.propTypes = {
   title: PropTypes.string.isRequired,
