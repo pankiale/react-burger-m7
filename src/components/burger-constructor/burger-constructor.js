592bff25518a13e374f527772d8aca215fdd170d
pankiale <93738391+pankiale@users.noreply.github.com>
2022-06-07 17:29:45 +0200

    review fixes

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import { dataTypes } from "../../utils/const";
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
-import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./burger-constructor.module.css";
 import IngredientSection from "../ingredient-section/ingredient-section";
+import TotalBill from './total-bill'
 
 BurgerConstructor.propTypes = {
   handleClick: PropTypes.func.isRequired,
@@ -15,19 +15,6 @@ function BurgerConstructor(props) {
   const handleClick = () => {
     props.handleClick();
   }
-
-  const TotalBill = ({ total }) => {
-    return (
-      <div className={styles.ingredients__total}>
-        <p className="text text_type_digits-medium">{total}</p>
-        <CurrencyIcon type="primary" />
-      </div>
-    );
-  };
-
-  TotalBill.propTypes = {
-    total: PropTypes.number.isRequired,
-  };
 
   return (
     <section className={`${styles.ingredients__section} pl-5 pr-4 pt-25`}>
